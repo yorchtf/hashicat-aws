ad28cfccffa4b021c6684f9d590734e0e23b7516
yorchtf <yorchtf@gmail.com>
2020-10-29 12:35:14 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Deparament = "devops"
+    Departament = "devops"
     Billable = "True"
   }
 }
